fcbe499934f48a49e27f7d0a841af9a8314b0039
KinerBoi <87410075+KinerBoi@users.noreply.github.com>
2023-04-05 10:12:48 -0700

    uwieduiewd

--- src/main/java/com/nighthawk/spring_portfolio/mvc/quiz/Quiz.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/quiz/Quiz.java
@@ -1,5 +1,37 @@
 package com.nighthawk.spring_portfolio.mvc.quiz;
 
 public class Quiz {
+
+    private String questions;
+    private String answers;
+    private int questionsRight;
+    private int questionstWrong;
+    private String right;
+    private String wrong;
     
+    public Quiz () {
+        this.questions = questions;
+        this.answers = answers;
+        this.percentRight = percentRight;
+        this.right = right;
+        this.wrong = wrong;
+    }
+
+    public String analysis (String questions, String answers) {
+
+        if (answers == 1) {
+
+        }
+        else if (answers == 0) {
+
+        }
+        else {
+
+        }
+
+    }
+
+     public static void main(String[] args) {
+
+     }
 }
